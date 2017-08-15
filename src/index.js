defeb415d46b8d9d86936ff9c07270eeeb821f24
pawaniu <pawangupta@Pawans-MacBook-Pro.local>
2017-08-15 09:59:56 +0530

    Simple API Implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import './index.css'
-import numeral from 'numeral';
+import './index.css';
 
-const courseValue = numeral(1000000000000).format('$0,0.00');
-console.log(`I would like to pay ${courseValue} for this awesome course!`);
+import {getUsers} from './api/userApi';
+
+// Populate table of users via API call.
+getUsers().then(result => { 
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
