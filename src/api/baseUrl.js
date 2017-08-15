566eeeaea36c444ae7cd960e5b709d58c15396ac
pawaniu <pawangupta@Pawans-MacBook-Pro.local>
2017-08-15 18:43:45 +0530

    deploy steps added

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl() {
     
    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' 
-   : '/';
+   : 'https://young-sierra-84597.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
